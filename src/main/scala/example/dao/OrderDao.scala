9e92401b12cd54133b56e486dbbda51787e77c08
Yuki Ishikawa <sobacode@gmail.com>
2019-11-08 03:19:55 +0900

    Fix compile error

--- src/main/scala/example/dao/OrderDao.scala
+++ src/main/scala/example/dao/OrderDao.scala
@@ -5,13 +5,14 @@ import cats.effect.IO
 import com.twitter.util.Future
 import doobie._
 import doobie.implicits._
+import doobie.refined.implicits._
 import doobie.util.transactor.Transactor
 import example.EnrichIO._
 import example.schema.Order
 import example.schema.values.OrderId
 
 class OrderDao(implicit xa: Transactor[IO]) {
-  private def create(order: Order): Future[Unit] = {
+  def create(order: Order): Future[Unit] = {
     import order._
     val q = sql"""|INSERT INTO `order` (order_id, symbol, side, quantity)
                   |VALUES (${id}, ${symbol}, ${side}, ${quantity})"""
@@ -21,7 +22,7 @@ class OrderDao(implicit xa: Transactor[IO]) {
       .unit
   }
 
-  private def findBy(orderId: OrderId): Future[Order] = {
+  def findBy(orderId: OrderId): Future[Order] = {
     sql"SELECT order_id, symbol, side, quantity FROM `order` WHERE order_id = ${orderId}"
       .query[Order]
       .unique
@@ -29,7 +30,7 @@ class OrderDao(implicit xa: Transactor[IO]) {
       .runToFuture
   }
 
-  private def selectBy(orderIds: NonEmptyList[OrderId]): Future[List[Order]] = {
+  def selectBy(orderIds: NonEmptyList[OrderId]): Future[List[Order]] = {
     val q =
       fr"SELECT order_id, symbol, side, quantity FROM `order` WHERE" ++
         Fragments.in(fr"order_id", orderIds) ++ fr"ORDER BY order_id"
